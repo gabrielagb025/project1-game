62c983a777ce5b20d9bdeb86ef1b5c265e83ad61
Gabriela García <ggarciaborregon@gmail.com>
2023-06-04 13:45:52 +0200

    changes on background and game

---
+++ background.js
@@ -0,0 +1,28 @@
+class Background {
+    constructor(ctx, imageSrc) {
+        this.ctx = ctx;
+        this.x = 0;
+        this.y = 0;
+        this.width = this.ctx.canvas.width;
+        this.height = this.ctx.canvas.height;
+        this.image = new Image();
+        this.image.src = "./images/FONDO-CON-EDIFICIOS.jpg";
+
+        this.image.onload = () => {
+            this.isReady = true;
+        }
+    }
+
+    draw() {
+        if (this.isReady) { 
+            this.ctx.drawImage(
+                this.image,
+                this.x,
+                this.y,
+                this.width,
+                this.height
+            )
+        }
+    }
+
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector("#my-canvas");
+const ctx = canvas.getContext("2d");
+
+const game = new Game(ctx);
+game.start();
